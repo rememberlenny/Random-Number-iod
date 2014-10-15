cbcba63728f7957f087e7eb9477b35fe889a3d02
RememberLenny <leonard_bogdonoff@condenast.com>
2014-10-15 09:44:13 -0400

    Initial Commit

---
+++ ATRRandomNumber/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ATRRandomNumber
+//
+//  Created by Leonard Bogdonoff on 10/15/14.
+//  Copyright (c) 2014 New Public Art Foundation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
