9db09592ce04708d0ce8fb2895c1566617bd7e8f
RememberLenny <leonard_bogdonoff@condenast.com>
2014-10-15 10:59:58 -0400

    9. Create the class for ATRItem

---
+++ ATRRandomNumber/ATRItem.h
@@ -0,0 +1,13 @@
+//
+//  ATRItem.h
+//  ATRRandomNumber
+//
+//  Created by Leonard Bogdonoff on 10/15/14.
+//  Copyright (c) 2014 New Public Art Foundation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ATRItem : NSObject
+
+@end
---
+++ ATRRandomNumber/ATRItem.m
@@ -0,0 +1,13 @@
+//
+//  ATRItem.m
+//  ATRRandomNumber
+//
+//  Created by Leonard Bogdonoff on 10/15/14.
+//  Copyright (c) 2014 New Public Art Foundation. All rights reserved.
+//
+
+#import "ATRItem.h"
+
+@implementation ATRItem
+
+@end
