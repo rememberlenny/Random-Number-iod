2c29f5902e907149b82b9e51ff21f84fb62bf222
RememberLenny <leonard_bogdonoff@condenast.com>
2014-10-16 11:53:09 +0530

    16. Overwrite the description method

--- ATRRandomNumber/ATRItem.m
+++ ATRRandomNumber/ATRItem.m
@@ -39,4 +39,17 @@
     return _dateCreated;
 }
 
+- (NSString *)description
+{
+    NSString *descriptionString =
+    [[NSString alloc] initWithFormat:@"%@ (%@): Worth $%d, recorded on %@",
+        self.itemName,
+        self.serialNumber,
+        self.valueInDollars,
+        self.dateCreated];
+    
+    return descriptionString;
+
+}
+
 @end
